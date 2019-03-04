e56c80d8c8e91bcfd0771d2c91e8ed6cf741bc01
n0364359 <michael.blydenburgh@libertymutual.com>
2019-03-04 17:46:20 -0500

    side nav bar, add employee modal started

--- components/customer_dash/Manage.js
+++ components/customer_dash/Manage.js
@@ -1,29 +1,36 @@
 import React, { Component } from 'react';
-import Styled from 'styled-components'
-import FullNav from '../FullNav';
-import Link from 'next/link';
-import Modal from '../Modal';
+import Styled from 'styled-components';
+import Button from '../Button';
+import Card from '../Card';
+import Input from '../Input';
 
  const Manage = ({employees}) => {
 
   const renderEmployees = (employeesArray) => {
     return employeesArray.map(employee=>{
       return (
-        <li>{`${employee.first_name} ${employee.last_name}`}</li>
+        <EmployeeCard>
+          <Card 
+            title={`${employee.first_name} ${employee.last_name}`}
+            body={
+            <div>
+              <p>{`Role: ${employee.role}`}</p>
+              <p>{`Email: ${employee.email}`}</p>
+              <p>{`Phone: ${employee.phone}`}</p>
+            </div>
+            }  />
+        </EmployeeCard>
       )
     })
   }
 
     return(
-      <React.Fragment>
-        <ManageDiv>
-          <h1>Manage Employees</h1>
-          <EmployeeListDiv>
-            {renderEmployees(employees)}
-          </EmployeeListDiv>
-        </ManageDiv>
-      </React.Fragment>
-      
+      <ManageDiv>
+        <h1>Manage Employees</h1>
+        <EmployeeListDiv>
+          {renderEmployees(employees)}
+        </EmployeeListDiv>
+      </ManageDiv>
     )
 }
 
@@ -33,16 +40,23 @@ const NavLink = Styled.a`
   margin:.5rem;
 `
 
+const AddForm = Styled.div`
+
+`;
+
 const ManageDiv= Styled.div`
   display: grid;
-  grid-template-columns: 65vw 1fr;
+  grid-template-columns: 1fr;
 
   >h1{
-    grid-column:1/-1;
     justify-self: center;
   }
 `
 
 const EmployeeListDiv = Styled.div`
+
+`;
+
+const EmployeeCard = Styled.div`
 
 `;
--- pages/dashboard/customer.js
+++ pages/dashboard/customer.js
@@ -5,6 +5,7 @@ import Home from '../../components/customer_dash/Home';
 import Input from '../../components/Input';
 import Manage from '../../components/customer_dash/Manage';
 import Modal from '../../components/Modal';
+import Nav from '../../components/Nav';
 import FullNav from '../../components/FullNav';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { CUSTOMERS_API, EMPLOYEES_API } from "../../static/api-config";
@@ -13,9 +14,16 @@ class Customer extends Component{
   state={
     showHome:true,
     showManage: false,
+    addModal:false,
     jwt:'',
     employees:[],
-    customerData:{}
+    customerData:{},
+    emailInput:{
+      value:'',
+      regex:/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      error:'Please enter a valid email',
+      isValid:false
+    }
   }
 
   findEmployeesByCompany = async (id) => {
@@ -32,6 +40,15 @@ class Customer extends Component{
     return employeesData;
   }
 
+  validateForm = (name,value) => {
+    const isValid = (this.state[name].regex.test(value)) ? true : false;
+    const updatedState = {...this.state[name]};
+    updatedState.isValid = isValid;
+    this.setState({
+      [name]:updatedState
+    })
+   }
+
   handleNavClick = (event) => {
     const { name } = event.target;
     switch(name){
@@ -46,7 +63,20 @@ class Customer extends Component{
           showHome:false,
           showManage:true
         })
+        break;
+      case "Add":
+        this.setState({
+          addModal:true
+        })
     }
+  }
+
+  handleInputChange = (event) => {
+
+  }
+
+  handleBlur = (event) => {
+
   }
 
   async componentDidMount(){
@@ -62,12 +92,32 @@ class Customer extends Component{
   render(){
     return(
       <React.Fragment>
-        <FullNav>
-          <Button type="green" onClick={this.handleNavClick} name="Home">Home</Button>
-          <Button type="green" onClick={this.handleNavClick} name="Manage">Manage</Button>
-        </FullNav>
-        {(this.state.showHome)?<Home employees={this.state.employees}/>: null}
-        {(this.state.showManage)?<Manage employees={this.state.employees} />: null}
+        <DashBody>
+          <NavDiv>
+            <Nav />
+          </NavDiv>
+          <ControlPanel>
+            <Button type="green" onClick={this.handleNavClick} name="Home">Home</Button>
+            <Button type="green" onClick={this.handleNavClick} name="Manage">Manage</Button>
+            {(this.state.showManage)?<Button type="blue" onClick={this.handleNavClick} name="Add">Add Employees</Button>:null}
+          </ControlPanel>
+          {(this.state.showHome)?<Home employees={this.state.employees}/>: null}
+          {(this.state.showManage)?<Manage employees={this.state.employees} />: null}
+        </DashBody>
+        <Modal
+          name="addModal"
+          buttonNames={["Add Employee"]}
+          show={this.state.addModal}
+          handleClose={this.handleNavClick}>
+          <h3>Add an Employee</h3>
+          <Input
+           type="text" 
+           placeholder="Enter Email" 
+           value={this.state.emailInput.value}
+           name="emailInput"
+           onChange={this.handleInputChange}
+           onBlur={this.handleBlur}/>
+        </Modal>
       </React.Fragment>
     )
   }
@@ -79,4 +129,22 @@ const NavLink = Styled.a`
   margin:.5rem;
   color: white;
   text-decoration: none;
+`
+
+const NavDiv = Styled.div`
+  grid-column:1/-1
+`;
+
+const DashBody = Styled.div`
+ display: grid;
+ height:100vh;
+ width:100vw;
+ grid-template-columns: 1fr 80vw;
+ grid-template-rows: auto 1fr;
+
+`
+
+const ControlPanel = Styled.div`
+  display: grid;
+  background-color: #333;
 `
