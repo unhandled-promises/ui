fbd6076c14713e35d05a434b0340c8cbd3e57b4a
Darren Anastasoff <soleman16@gmail.com>
2019-03-13 20:22:22 -0400

    updated navbar styling and some minor tweaks to the employee dashboard

--- components/FullNav.js
+++ components/FullNav.js
@@ -4,7 +4,7 @@ import Styled from 'styled-components';
 export default ({sticky,children}) => {
 	return (
 		<Nav sticky={sticky} items={children}>
-			<h1>App Name</h1>
+			<h1>Fit2Work</h1>
       {children}
 		</Nav>
 	)
@@ -14,14 +14,21 @@ const Nav = Styled.div`
 	width: 100%;
 	position:${({sticky})=>(sticky)?"sticky":"block"};
 	top:0;
-	background-color: #9FBEBA;
+	font-family: 'Baloo Chettan', cursive;
+	font-size: 30px;
+	background-color: #1f2d3f;
 	margin-bottom:0;
 	color: #ffffff;
+	opacity: .9;
 	overflow:auto;
   display: grid;
   grid-template-columns: ${({items})=>`1fr repeat(${items.length},auto)`};
   align-items:center;
-  font-size: 1.2rem;
+	font-size: 1.2rem;
+	
+	h1{
+		margin-left: 10px;
+	}
 
   >a:hover{
     color:#eee;
