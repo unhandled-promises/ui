9718457db59badd7a305f25fc8837b43c51e303e
Darren Anastasoff <soleman16@gmail.com>
2019-03-18 12:07:44 -0400

    initial checkin of adding the sidebar to the employee dashboard

--- pages/dashboard/employee.js
+++ pages/dashboard/employee.js
@@ -1,22 +1,20 @@
 import React, { Component } from 'react';
 import Styled from 'styled-components';
 import Footer from '../../components/Footer';
-import FullNav from '../../components/FullNav';
-import Input from '../../components/Input';
-import Modal from '../../components/Modal';
 import { EMPLOYEES_API } from "../../static/api-config";
-import Link from 'next/link';
 import Router from 'next/router';
 import Badges from '../../components/Badges'
 import Friends from '../../components/Friends'
 import LifetimeStats from '../../components/LifetimeStats'
 import EmployeeCard from '../../components/EmployeeCard'
 import { Container, Row, Col } from 'styled-bootstrap-grid';
-import DailyStats from '../../components/DailyStats';
 import Devices from '../../components/Devices';
 import TimeSeriesBarChart from '../../components/TimeSeriesBarChart';
 import Health from '../../components/Health';
 import SideBar from "../../components/SideBar";
+import EmployeeInfo from "../../components/EmployeeInfo";
+import { withStyles } from "@material-ui/core/styles";
+import Modal from '../../components/Modal';
 
 class Employee extends Component {
 	state = {
@@ -54,40 +52,16 @@ class Employee extends Component {
 		friends: { friends: [] },
 		steps: { 'activities-steps': [] },
 		distance: { 'activities-distance': [] },
-		editModal: false,
 		jwt: '',
+		displayEmployeeInfo: false,
+		displayCompanyInfo: false,
 		employeeData: {
-
 			firstName: '',
 			lastName: '',
 			password: '',
 			phone: '',
 			dob: '',
 			email: ''
-		},
-		firstNameInput: {
-			value: '',
-			regex: /^[a-z]+$/i,
-			error: "Please enter a valid first name",
-			isValid: true
-		},
-		lastNameInput: {
-			value: '',
-			regex: /^[a-z]+$/i,
-			error: "Please enter a valid last name",
-			isValid: true
-		},
-		passwordInput: {
-			value: '',
-			regex: /[\w]+/,
-			error: "Please enter a valid password at least 8 characters in length",
-			isValid: true
-		},
-		phoneInput: {
-			value: '',
-			regex: /(^([\d]{3}\-){2})[\d]{4}$/,
-			error: 'Please enter a phone number in 555-555-5555 format',
-			isValid: true
 		},
 		dateInput: {
 			value: ''
@@ -130,143 +104,57 @@ class Employee extends Component {
 		};
 	}
 
-	handleInputChange = (event) => {
-		const { name, value } = event.target;
-		console.log(value);
-		const prevState = { ...this.state[name] };
-		prevState.value = value;
-		this.setState({
-			[name]: prevState,
-		})
-	}
-
-	handleBlur = async (event) => {
-		console.log(event.target);
-		const { name } = event.target;
-		const value = this.state[name].value;
-		switch (name) {
-			case "firstNameInput":
-				await this.validateForm(name, value);
-				console.log(this.state.firstNameInput.isValid);
-				break;
-			case "lastNameInput":
-				await this.validateForm(name, value);
-				console.log(this.state.lastNameInput.isValid);
-				break;
-			case "passwordInput":
-				await this.validateForm(name, value);
-				console.log(this.state.passwordInput.isValid);
-				break;
-			case "phoneInput":
-				await this.validateForm(name, value);
-				console.log(this.state.phoneInput.isValid);
-				break;
+	async handlerInfo(value) {
+		const employee = {
+			firstName: value.firstName,
+			lastName: value.lastName,
+			dob: value.date,
+			phone: value.phone,
+			id: this.state.id,
+			password: value.password,
 		}
+
+		console.log(employee);
+		await this.updateEmployeeInformation(employee)
+		this.handleNext();
 	}
 
-	validateForm = (name, value) => {
-		const isValid = (this.state[name].regex.test(value)) ? true : false;
-		const updatedState = { ...this.state[name] };
-		updatedState.isValid = isValid;
-		this.setState({
-			[name]: updatedState
-		})
-	}
-
-	updateAccountInfo = async (updatedInfo) => {
-		console.log("Updating User")
-		const { id, firstName, lastName, password, phone, dob } = updatedInfo;
-		const { jwt } = this.state;
-		const updateResponse = await fetch(`${EMPLOYEES_API}api/employee/${id}`, {
+	updateEmployeeInformation = async (employee) => {
+		const first = employee.firstName;
+		const last = employee.lastName;
+		const dob = employee.dob;
+		const phone = employee.phone;
+		const password = employee.password;
+		const employeeResponse = await fetch(`${EMPLOYEES_API}api/employee/${employee.id}`, {
 			method: "PUT",
 			body: JSON.stringify({
-				"first_name": firstName,
-				"last_name": lastName,
-				"password": password,
+				"first_name": first,
+				"last_name": last,
+				"dob": dob,
 				"phone": phone,
-				"dob": dob
+				"password": password
 			}),
 			headers: {
-				"Authorization": jwt,
-				"Content-type": "application/json"
-			}
+				"Authorization": sessionStorage.getItem("jwt"),
+				"Content-Type": "application/json"
+			},
 		})
-		const updateData = await updateResponse.json();
-		console.log(updateData);
+
+		const employeeData = employeeResponse.json();
+		return employeeData;
 	}
 
-	logUserOut = async () => {
-		console.log(`Logging user out and removing jwt from session storage`);
-		await sessionStorage.removeItem("jwt");
-		Router.push("/login");
-	}
-
-	handleClick = async (event) => {
-		const { name } = event.target;
-		switch (name) {
-			case "edit":
-				const { id } = this.state.employeeData;
-				const { first_name: firstName, last_name: lastName, password, phone, dob } = this.state.employeeData;
-				const prevFirstNameState = { ...this.state.firstNameInput };
-				const prevLastNameState = { ...this.state.lastNameInput };
-				const prevPhoneState = { ...this.state.phoneInput };
-				const prevDobState = { ...this.state.dateInput };
-				const prevPasswordState = { ...this.state.passwordInput };
-
-				prevFirstNameState.value = firstName;
-				prevLastNameState.value = lastName;
-				prevPasswordState.value = password;
-				prevDobState.value = dob;
-				prevPhoneState.value = phone;
-
-				this.setState({
-					firstNameInput: prevFirstNameState,
-					lastNameInput: prevLastNameState,
-					passwordInput: prevPasswordState,
-					dateInput: prevDobState,
-					phoneInput: prevPhoneState
-				});
-				this.setState({
-					editModal: true
-				})
-				break;
-			case "editModal":
-				this.setState({
-					editModal: false
-				})
-				break;
-			case "Update":
-				const { firstNameInput, lastNameInput, phoneInput, passwordInput, dateInput } = this.state;
-				const { _id: employeeId } = this.state.employeeData;
-				console.log(`id: ${employeeId}`);
-				const validInfo = firstNameInput.isValid && lastNameInput.isValid && passwordInput.isValid;
-				if (validInfo) {
-					const updatedInfo = {
-						firstName: firstNameInput.value,
-						lastName: lastNameInput.value,
-						password: passwordInput.value,
-						phone: phoneInput.value,
-						dob: dateInput.value,
-						id: employeeId
-					}
-					console.log(updatedInfo);
-					await this.updateAccountInfo(updatedInfo);
-					this.setState({
-						editModal: false
-					})
-				}
-				else {
-					console.log(`Please enter valid information`);
-				}
-				break;
-			case "Logout":
-				this.logUserOut();
-				break;
-
+	performAction = async (navType) => {
+		if ("editEmployee" === navType) {
+			await this.setState({ displayEmployeeInfo: true });
+		} else if ("editCompany" === navType) {
+			await this.setState({ displayCompanyInfo: true });
 		}
 	}
 
 	render() {
+		const { classes } = this.props;
+
 		let caloriesOut = "N/A";
 		let restingHeartRate = "N/A";
 
@@ -276,13 +164,11 @@ class Employee extends Component {
 		if (this.state.todayStats && this.state.todayStats.summary && this.state.todayStats.summary.restingHeartRate) {
 			restingHeartRate = this.state.todayStats.summary.restingHeartRate;
 		};
+
+		console.log("displayEmployeeInfo: ", this.state.displayEmployeeInfo);
 		return (
 			<React.Fragment>
-				<SideBar>
-					{/* <FullNav>
-						<Link href="/dashboard/employee"><NavLink>Home</NavLink></Link>
-						<NavLink onClick={this.handleClick} name="Logout">Logout</NavLink>
-					</FullNav> */}
+				<SideBar onClickIcon={this.performAction}>
 					<LoginDiv>
 						<Container>
 							<Title>Your Personal Health Dashboard</Title>
@@ -320,18 +206,15 @@ class Employee extends Component {
 								</Col>
 							</Row>
 						</Container>
-						<Modal name="editModal"
-							buttonNames={["Update"]}
-							show={this.state.editModal}
-							handleClick={this.handleClick}
-							handleClose={this.handleClick}>
-							<h3>Edit Your Information</h3>
-							<Input type="text" name="firstNameInput" placeholder="First Name" value={this.state.firstNameInput.value} onChange={this.handleInputChange} onBlur={this.handleBlur} />
-							<Input type="text" name="lastNameInput" placeholder="Last Name" value={this.state.lastNameInput.value} onChange={this.handleInputChange} onBlur={this.handleBlur} />
-							<Input type="password" name="passwordInput" placeholder="Password" value={this.state.passwordInput.value} onChange={this.handleInputChange} onBlur={this.handleBlur} />
-							<Input type="text" name="phoneInput" placeholder="Phone Number" value={this.state.phoneInput.value} onChange={this.handleInputChange} onBlur={this.handleBlur} />
-							<Input type="date" name="dateInput" placeholder="Date of Birth" value={this.state.dateInput.value} onChange={this.handleInputChange} />
-						</Modal>
+						<InfoDiv shouldDisplay={this.state.displayEmployeeInfo}>
+							<Modal name="editModal"
+								buttonNames={[]}
+								show={this.state.displayEmployeeInfo}
+								handleClick={this.handleClick}
+								handleClose={this.handleClick}>
+								<EmployeeInfo classes={classes} handler={this.handlerInfo} scope="update" employeeId={this.state.employeeData.id} jwt={this.state.jwt} />
+							</Modal>
+						</InfoDiv>
 					</LoginDiv>
 				</SideBar>
 				<Footer />
@@ -340,7 +223,7 @@ class Employee extends Component {
 	}
 }
 
-export default Employee;
+export default withStyles(styles)(Employee);
 
 const LoginDiv = Styled.div`
     grid-template-columns: 2fr;
@@ -358,31 +241,32 @@ const LoginDiv = Styled.div`
 	display: grid;
 	text-align: center
 `
-
-const NavLink = Styled.a`
-	margin:.5rem;
-	color: white;
-	text-decoration: none;
-	cursor: pointer;
-`
+const styles = theme => ({
+	tieredWrap: {
+		display: "flex",
+		flexDirection: "row",
+		flexWrap: "wrap",
+		justifyContent: "space-between",
+		alignItems: "center",
+	},
+})
 
 const Title = Styled.h2`
 	text-align: center;
 	color: grey;
 `
-
-const WelcomeDiv = Styled.div`
-	text-align:center;
-`
-
-const CardWrapper = Styled.div`
-	display: grid;
-	grid-gap: 20px;
-	margin:20px;
-	grid-template-columns: repeat(3, 1fr);
-`
-
-const ButtonDiv = Styled.div`
-	margin: auto;
-	width: 50vw;
+const InfoDiv = Styled.div`
+    grid-template-columns: 2fr;
+    max-width: 800px;
+    width:80%;
+    padding:30px;
+    margin:40px auto;
+    background: #FFF;
+    border-radius: 10px;
+    -webkit-border-radius:10px;
+    -moz-border-radius: 10px;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.13);
+    -moz-box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.13);
+    -webkit-box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.13);
+	display:${({ shouldDisplay }) => (shouldDisplay === true) ? "grid" : "none"}
 `
