b4744958c4e182cafef5a3606d6c24019a1bae29
Green Man <brendan.mccaughey@gmail.com>
2019-03-17 21:52:45 -0400

    Layout redesign. Added clip paths to sections and images.

--- pages/index.js
+++ pages/index.js
@@ -17,31 +17,40 @@ export default () => {
   return (
     <Body>
       <Nav sticky={false} />
-      <Main>
+      <Section1>
         <JoinDiv>
           <h1>Fit2Work is the next level of employee wellness.</h1>
           <Link href="/signup"><Create type="dark">Create an Account</Create></Link>
           <h4>Already a member? <Link href="/login"><a>Login</a></Link></h4>
         </JoinDiv>
+        </Section1>
+        <Section2>
         <Testimonials>
-          <h2>TESTIMONIALS</h2>
+          <h1>TESTIMONIALS<hr/></h1>
           <p>Here's what some of our clients are saying about us!</p>
           <TestCard1 title="Much Health" body={placeholder1} />
           <TestCard2 title="So Fit" body={placeholder2} />
           <TestCard3 title="Such Safe" body={placeholder3} />
         </Testimonials>
+        </Section2>
+        <Section3>
         <How>
-          <h2>HOW IT WORKS</h2>
-          <p>Having a healthy, happy workforce is one of the keys to increased productivity and efficiency. 
+          <h1>HOW IT WORKS<hr/></h1>
+          <h4>Having a healthy, happy workforce is one of the keys to increased productivity and efficiency. 
             Fit2Work allows companies and their employees to link their fitness tracker device to our online display dashboard. 
             Utilizing the Fitbit API, employees are able to see their heart rate, steps, and calories burned among other statistics. Company 
-            owners can view employee heart rate and will be able to know if a safety action is needed. Magna adipisicing voluptate aliqua quis aliqua quis culpa sunt ex occaecat commodo dolore laborum fugiat. Ullamco occaecat non laborum sint nisi. Irure non sunt et exercitation sit commodo aute tempor. Duis in enim enim veniam enim in occaecat ad officia esse mollit id. Consequat qui veniam laborum amet commodo. Eiusmod ullamco quis amet laborum aliquip fugiat consectetur irure ut sunt.</p>
+            owners can view employee heart rate and will be able to know if a safety action is needed.<br/>
+            In eu veniam nostrud irure. Mollit elit adipisicing culpa irure ipsum voluptate ullamco. Officia proident do in cupidatat cillum qui voluptate pariatur do dolore culpa cupidatat non nostrud.
+            Ullamco consectetur cupidatat fugiat deserunt. In reprehenderit qui cupidatat deserunt qui. Incididunt officia fugiat esse excepteur consectetur occaecat excepteur exercitation ipsum enim labore esse. Eiusmod occaecat ad aute id amet et fugiat cupidatat. Aliqua minim excepteur occaecat labore nulla. Tempor id incididunt consectetur sint exercitation nostrud ad veniam pariatur enim occaecat.
+            </h4>
           <Wrap>
           <img src="/static/images/athlete-bike.jpg" />
           </Wrap>
         </How>
+        </Section3>
+        <Section4>
         <Contact>
-          <h2>CONTACT US</h2>
+          <h1>CONTACT US<hr/></h1>
           <p>Curious? Reach out and request a demo!</p>
           <form>
           <ContactInput type="name" placeholder="Name"/>
@@ -52,35 +61,38 @@ export default () => {
           <Wrap>
           <img src="/static/images/yoga.jpeg"/>
           </Wrap>
-        </Contact>
-      </Main>
+        </Contact>    
+        </Section4> 
       <Footer />
     </Body>
   )
 }
 
 const Body = Styled.div`
-  background: url("/static/images/endurance-exercise-female.jpg") no-repeat fixed center;
   display: grid;
-  grid-template-rows: 100px auto 1fr;
+  grid-template-rows: 100px 100vh 90vh 90vh 80vh 1fr;
+  font-family: 'Montserrat', sans-serif;
+  background: url("/static/images/endurance-exercise-female.jpg") no-repeat fixed center;
 `
 
 const Main = Styled.main`
   display:grid;
   grid-template-rows: 300px 400px 500px 500px;
-  grid-gap:500px;
   padding: 1rem;
   font-family: 'Montserrat', sans-serif;
   margin-top:100px;
-
-  h1,h2,h3,h4 {
-    font-family: 'Roboto', sans-serif;
-  }
-
 `
+const Section1 = Styled.div `
+background: url("/static/images/endurance-exercise-female.jpg") no-repeat fixed center;
+display: grid;
+grid-template-rows: 150px 350px 100px;
+grid-template-areas: ". . ." ". join ." ". . .";
+`
+
 const JoinDiv = Styled.div`
   background: linear-gradient(to bottom right, #fed75e, #FFA600);
   max-width: 30vw;
+  grid-area: join;
   display: grid;
   border-radius: 25px;
   opacity: .9;
@@ -104,8 +116,17 @@ const Create = Styled(Button)`
     background-color: #131B26;
   }
 `
+const Section2 = Styled.div `
+  background: #FFA600;
+  display: grid;
+  grid-template-rows: 150px 350px;
+  grid-template-areas: ". . ." ". test .";
+  -webkit-clip-path: polygon(50% 22%, 100% 0, 100% 75%, 50% 100%, 0% 75%, 0 0);
+  clip-path: polygon(50% 22%, 100% 0, 100% 75%, 50% 100%, 0% 75%, 0 0);
+`
 
 const Testimonials = Styled.div`
+  grid-area:test;
   display: grid;
   grid-template-rows: repeat (3, 1fr);
   grid-template-columns: repeat(3, 1fr);
@@ -114,12 +135,10 @@ const Testimonials = Styled.div`
                          "card1 card2 card3";
   padding: 1rem;
   grid-gap: .75rem;
-  background: linear-gradient(to bottom right, #fed75e, #FFA600);
-  border-radius: 25px;
   justify:center;
   text-align:center;
 
-  h2 {
+  h1 {
     grid-area: title;
   }
   p {
@@ -139,9 +158,18 @@ const TestCard3 = Styled(Card)`
   grid-area:card3;
   background-color: #DCDCDC;
 `
+const Section3 = Styled.div `
+  background: #FFA600;
+  display: grid;
+  grid-template-rows: 150px 350px 100px;
+  grid-template-areas: ". . ." ". how .";
+  -webkit-clip-path: polygon(50% 22%, 100% 0, 100% 75%, 50% 100%, 0% 75%, 0 0);
+  clip-path: polygon(50% 22%, 100% 0, 100% 75%, 50% 100%, 0% 75%, 0 0);
+`
+
 const How = Styled.div `
-  background: linear-gradient(to bottom right, #fed75e, #FFA600);
-  border-radius: 25px;
+  grid-area:how;
+  background: #FFA600;
   text-align:center;
   display: grid;
   grid-gap: .75rem;
@@ -150,11 +178,12 @@ const How = Styled.div `
                       "message img"
                       "message img";
 
-  h2 {
+  h1 {
     grid-area: title;
+    text-align:left;
   }
 
-  p {
+  h4 {
     grid-area: message;
     margin: 20px;
     text-align:left;
@@ -164,20 +193,31 @@ const How = Styled.div `
     grid-area: img;
     margin: auto;
     width: 100%;
+    -webkit-clip-path: polygon(100% 0, 100% 66%, 0 100%, 0 34%);
+    clip-path: polygon(100% 0, 100% 66%, 0 100%, 0 34%);
   }
 `
+const Section4 = Styled.div `
+  background: #FFA600;
+  display: grid;
+  grid-template-rows: 150px 350px;
+  grid-template-areas: ". . ." "contact contact .";
+  -webkit-clip-path: polygon(50% 22%, 100% 0, 100% 75%, 50% 100%, 0% 75%, 0 0);
+  clip-path: polygon(50% 22%, 100% 0%, 100% 100%, 50% 100%, 0% 100%, 0 0);
+`
+
 const Contact = Styled.div `
-  background: linear-gradient(to bottom right, #fed75e, #FFA600);
-  border-radius: 25px;
+  grid-area: contact;
+  background: #FFA600;
   text-align:center;
   display: grid;
   grid-gap: .75rem;
-  grid-template-areas: "title img"
-                      "header img"
-                      "message img"
-                      "message img";
+  grid-template-areas: "img title"
+                      "img header"
+                      "img message"
+                      "img message";
 
-  h2 {
+  h1 {
     grid-area: title;
     margin:auto;
   }
@@ -196,8 +236,9 @@ const Contact = Styled.div `
   
   img {
     grid-area: img;
-    margin: auto;
     width: 100%;
+    -webkit-clip-path: polygon(100% 34%, 100% 100%, 0 66%, 0 0);
+    clip-path: polygon(100% 34%, 100% 100%, 0 66%, 0 0);
   }
 `
 const ContactInput = Styled(Input)` 
